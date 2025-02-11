b23b2b8d1484dc6368fccdced933834e7c21051e
Eli VanderMolen <sheldonvandermolen@gmail.com>
2025-02-11 10:34:00 -0600

    Changed footer message

--- public/footer.php
+++ public/footer.php
@@ -2,7 +2,7 @@
 </body>
 <!--footer message-->
 <div class="framed neutral">
-This website was created by Eli VanderMolen. Thank you for viewing my website.<br>
+This website is lovingly maintained by generous contributors. Thank you for viewing our website.<br>
 <i> Articles on this website may or may not have false information </i>
 </div>
 
