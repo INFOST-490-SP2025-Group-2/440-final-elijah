17e1d63b2343b3b7146e868dcef4b95eeb9c19f4
Barry Molina <bazzaboy@gmail.com>
2025-02-11 09:39:17 -0600

    add migragtions folder, db import works

--- public/mysqli_connect.php
+++ public/mysqli_connect.php
@@ -1,10 +1,14 @@
 <?php
 //This sets the constants with our connection information
-DEFINE ('DB_HOST', 'localhost'); //Database server -- Typically "localhost"
-DEFINE ('DB_USER', 'vande433_FinalProject'); //Database User Name
-DEFINE ('DB_PASSWORD', 'R34dy2B3D0n3'); //Database User Password
-DEFINE ('DB_NAME', 'vande433_FinalProject'); //Database Name
+// DEFINE ('DB_HOST', 'localhost'); //Database server -- Typically "localhost"
+// DEFINE ('DB_USER', 'vande433_FinalProject'); //Database User Name
+// DEFINE ('DB_PASSWORD', 'R34dy2B3D0n3'); //Database User Password
+// DEFINE ('DB_NAME', 'vande433_FinalProject'); //Database Name
+DEFINE('DB_USER', 'infost490_group2');
+DEFINE('DB_PASSWORD', 'password');
+DEFINE('DB_HOST', 'db');
+DEFINE('DB_NAME', 'infost490_group2_db');
 
 //This connects us to the database
-$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die 
+$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die 
 ('Could not connect to MySQL server with error: ' . mysqli_connect_error());
